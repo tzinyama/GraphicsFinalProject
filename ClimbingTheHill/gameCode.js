886f3263f54bf718c790525c43300e3b73f3cc62
a0rosenthal <a0rosenthal@gmail.com>
2018-04-11 23:10:24 -0400

    Prevented continual jumping when holding jump key, refactored code

--- ClimbingTheHill/gameCode.js
+++ ClimbingTheHill/gameCode.js
@@ -1,14 +1,6 @@
-
-/* This is a starter file for experimenting with 3D animated models in three.js.
- * The user can rotate the model using the keyboard, and can turn animation on and off.
-
- * As an example, it shows  a sphere rotating around a cube.
- *
- * To make your own model, add any global variables that you need for animating the model,
- * build the model in the createWorld() function, and update the animation variables in
- * the updateForFrame() function.
- *
- */
+//Computer Graphics final project
+//Alex Rosenthal, Bryan Vaihinger, Tino Zinyama
+//Climbing the Hill
 
 "use strict";
 
@@ -63,7 +55,7 @@ function createScene() {
   scene = new THREE.Scene();
 
   // create a camera, sitting on the positive z-axis.  The camera is not part of the scene.
-  camera = new THREE.PerspectiveCamera(45, canvas.width/canvas.height, 1, 30);
+  // camera = new THREE.PerspectiveCamera(45, canvas.width/canvas.height, 1, 30);
   camera = new THREE.OrthographicCamera(-12, 12, 9, -9, 1, 30);
   camera.position.z = 15;
 
@@ -85,16 +77,6 @@ function createScene() {
   // var model = createTree();
   //
   // models[STARTER] = model;
-  //
-  // // create another model
-  // var axleModel = createAxle();
-  // models[AXLE] = axleModel;
-  //
-  // var car = createCar(axleModel);
-  //
-  // // Needs to be bigger when it's displayed alone. Need to be done after the car creation!
-  // axleModel.scale.set(2,2,2);
-  // models[CAR] = car;
 
   var world = createWorld();
   models[WORLD] = world;
@@ -102,9 +84,6 @@ function createScene() {
   models[currentModel].position.y -= 2
   scene.add(models[WORLD]);
 }
-
-
-
 
 function createWorld() {
    // Create the game world
@@ -170,23 +149,11 @@ function render() {
  *  for that frame.
  */
 function updateForFrame() {
-  // if (currentModel == AXLE) {
-  //   models[AXLE].rotation.z += 0.05;
-  // }
-  // else if (currentModel == CAR) {
-  //   var ax1 = models[CAR].children[0];
-  //   var ax2 = models[CAR].children[1];
-  //   ax1.rotation.z += 0.05;
-  //   ax2.rotation.z += 0.05;
-  // }
+
   if (currentModel == WORLD) {
     clock = (clock + 1)%1000000;
     hero.update();
-    //hero.model.position.y += hero.hHeight/2;
   }
-  // } else {
-  //   sphereRotator.rotation.y += 0.03;
-  // }
 }
 
 
@@ -314,161 +281,3 @@ function init() {
   render();
   doAnimateCheckbox();
 }
-
-//
-//
-// // Older code
-//
-// // what is it?
-// function createStarter() {
-//
-//   var model = new THREE.Object3D();
-//
-//   model.add( new THREE.Mesh(  // add a big cube at the origin; the cube is not animated
-//       new THREE.BoxGeometry(3,3,3),
-//     new THREE.MeshLambertMaterial({ color: 0xff7700 })
-//   ));
-//
-//   model.add( new THREE.Mesh(  // A tall narrow cylinder on the axis of rotation
-//         new THREE.CylinderGeometry(0.3,0.3,8),
-//     new THREE.MeshLambertMaterial({ color: 0x0000AA })
-//   ));
-//
-//   var sphere = new THREE.Mesh(
-//       new THREE.SphereGeometry(1,32,16),
-//     new THREE.MeshPhongMaterial({
-//          color: "green",
-//       specular: 0x101010,
-//       shininess: 32
-//     })
-//   );
-//   sphere.position.x = 4;
-//   sphereRotator = new THREE.Object3D();
-//   sphereRotator.add(sphere);
-//   model.add(sphereRotator);
-//
-//   // Tip it forward a bit, so we're not looking at it edge-on.
-//   model.rotation.set(0.2,0,0);
-//   return model;
-// }
-//
-//
-// // not best but because part of Axle and also Headlight
-//   var yellow = new THREE.MeshPhongMaterial({
-//         color: 0xffff00,
-//       specular: 0x101010,
-//       shininess: 16
-//     });
-//
-//
-// function createAxle() {
-//   // Create the wheels and axles.
-//
-//   // Tire, the wheel object also contains the spokes
-//   var wheel = new THREE.Mesh(
-//     new THREE.TorusGeometry(0.75, 0.25, 16, 32),
-//     new THREE.MeshLambertMaterial({ color: 0x0000A0 })
-//   );
-//
-//
-//   // a yellow cylinder with height 1 and diameter 1
-//   var cylinder = new THREE.Mesh(
-//       new THREE.CylinderGeometry(0.5,0.5,1,32,1),
-//     yellow
-//   );
-//   // Make it thin and long for use as a spoke
-//   cylinder.scale.set(0.15,1.2,0.15);
-//   // Add a copy of the cylinder
-//   wheel.add(cylinder.clone());
-//   // Add a rotation about the z-axis for the second spoke
-//   cylinder.rotation.z = Math.PI/3;
-//   wheel.add(cylinder.clone());
-//   // For third spoke, use a negative rotation about z-axis
-//   cylinder.rotation.z = -Math.PI/3;
-//   wheel.add(cylinder.clone());
-//
-//   // A model containing two wheels and a cylinder.
-//   var axleModel = new THREE.Object3D();
-//   // scale the cylinder for use as an axle
-//   cylinder.scale.set(0.2,4.3,0.2);
-//   // rotate its axis onto the z-axis
-//   cylinder.rotation.set(Math.PI/2,0,0);
-//   axleModel.add(cylinder);
-//
-//   // the wheels are positioned at the top and bottom of cylinder
-//   wheel.position.z = 2;
-//   axleModel.add(wheel.clone());
-//   wheel.position.z = -2;
-//   axleModel.add(wheel);
-//
-//   return axleModel;
-// }
-//
-// function createCar(axleModel) {
-//   // Create a car, consisting of two boxes, two spheres for the headlights, and two axles.
-//   var carModel = new THREE.Object3D();
-//   var red = new THREE.MeshPhongMaterial({
-//        color: "red",
-//        specular: 0x080808,
-//        shininess: 8,
-//        shading: THREE.FlatShading
-//   });
-//
-//    var body = new THREE.Mesh(new THREE.BoxGeometry(6,1.2,3), red);
-//    body.position.y = 0.6;
-//
-//    var hood = new THREE.Mesh(new THREE.BoxGeometry(3,1,2.8), red);
-//    hood.position.set(0.5,1.4,0);
-//
-//    var headlight1 = new THREE.Mesh(new THREE.SphereGeometry(1,16,8), yellow);
-//    headlight1.scale.set(0.1,0.25,0.25);
-//    headlight1.position.set(-3,0.6,-1);
-//    var headlight2 = headlight1.clone();
-//    headlight1.position.set(-3,0.6,1);
-//
-//    // need to retrieve to animate
-//    var carAxle1 = axleModel.clone();
-//    carAxle1.position.x = -2.5;
-//    var carAxle2 = axleModel.clone();
-//    carAxle2.position.x = 2.5;
-//
-//    // 0 and 1st children of carModel!
-//    carModel.add(carAxle1);
-//    carModel.add(carAxle2);
-//    carModel.add(body);
-//    carModel.add(hood);
-//    carModel.add(headlight1);
-//    carModel.add(headlight2);
-//    return carModel;
-// }
-//
-//
-// function createTree() {
-//
-//
-// var tree = new THREE.Object3D();
-//
-// var trunk = new THREE.Mesh(
-//     new THREE.CylinderGeometry(0.2,0.2,1,16,1),
-//     new THREE.MeshLambertMaterial({
-//         color: 0x885522
-//     })
-// );
-// trunk.position.y = 0.5;  // move base up to origin
-//
-// var leaves = new THREE.Mesh(
-//     new THREE.ConeGeometry(.7,2,16,3),
-//     new THREE.MeshPhongMaterial({
-//         color: 0x00BB00,
-//         specular: 0x002000,
-//         shininess: 5
-//     })
-// );
-// leaves.position.y = 2;  // move bottom of cone to top of trunk
-//
-// tree.add(trunk);
-// tree.add(leaves);
-// return tree;
-//
-// }
-// //----------------------------------------------------------------------------------
