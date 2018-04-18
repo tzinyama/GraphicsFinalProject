a57f0f7c6e9a07cb802c05c86536f026b918c258
Tino Zinyama <tzinyama@colgate.edu>
2018-04-18 15:36:21 -0400

    fixed level tiling; added stone platforms to level; fixed camera placement

--- Levels/levels.js
+++ Levels/levels.js
@@ -1,7 +1,7 @@
-var WIDTH = 1000;
-var HEIGHT = 500;
+var WIDTH = 800;
+var HEIGHT = 600;
 
-var NUM_GRID_ROWS = 10;
+var NUM_GRID_ROWS = 15;
 var NUM_GRID_COLS = 20;
 
 var CELL_WIDTH = WIDTH / NUM_GRID_COLS;
@@ -17,20 +17,78 @@ var LIVE_PLATFORM = "-";
 var LIVE_PLATFORM_START = '<';
 var LIVE_PLATFORM_END = '>';
 
+//------------------------------Functions to create ----------------------------//
+
+function createStonePlatform(){
+  var model = new THREE.Object3D();
+
+  var geomBase = new THREE.BoxGeometry(4,4.3,4);
+  var matBase = new THREE.MeshPhongMaterial(
+                          {color:0x888888, shading:THREE.FlatShading });
+  model.base = new THREE.Mesh(geomBase,matBase);
+  model.add(model.base);
+
+  var matStone1 = new THREE.MeshPhongMaterial(
+                          {color:0x506065, shading:THREE.FlatShading });
+  var matStone2 = new THREE.MeshPhongMaterial(
+                          {color:0x888888, shading:THREE.FlatShading });
+
+  var geomBrick1 = new THREE.BoxGeometry(2,1.2,1.5);
+  var brick1 = new THREE.Mesh(geomBrick1, matStone1);
+  brick1.position.z = 1.5;
+  brick1.position.x = -.75;
+  brick1.position.y = 1.5;
+  model.add(brick1.clone());
+  brick1.scale.set(.5,1,1);
+  brick1.position.x += 2;
+  model.add(brick1.clone());
+  brick1.scale.set(1.3,1.2,1);
+  brick1.position.y -= 1.5;
+  brick1.position.x -= .5;
+  model.add(brick1.clone());
+  brick1.scale.set(.4,1.2,1);
+  brick1.position.x -= 2;
+  model.add(brick1.clone());
+  brick1.scale.set(1.5,.75,1);
+  brick1.position.y-=1.4;
+  brick1.position.x += .75;
+  model.add(brick1.clone());
+  brick1.scale.set(.3,.75,1);
+  brick1.position.x += 2;
+  model.add(brick1.clone());
+  brick1.scale.set(1.3,1,1);
+  brick1.position.y = 1.5;
+  brick1.position.z = -.5;
+  brick1.position.x -= .85;
+  model.add(brick1.clone());
+  brick1.scale.set(.5,1,1);
+  brick1.position.x -= 2;
+  model.add(brick1.clone());
+
+
+  model.position.y = 5;
+  model.position.x = -8;
+
+
+  model.scale.set(10,10,10);
+  return model;
+}
+
 //------------------------------Pre-made game objects ----------------------------//
 
-var platform = new THREE.Mesh(
-  new THREE.BoxGeometry(CELL_WIDTH, CELL_HEIGHT/2, 0),
-  new THREE.MeshLambertMaterial( { color: 0x00CC55 } )
-);
+var platform = createStonePlatform();
+// var platform = new THREE.Mesh(
+//   new THREE.BoxGeometry(CELL_WIDTH, CELL_HEIGHT/2, 0),
+//   new THREE.MeshLambertMaterial( { color: 0x00CC55 } )
+// );
 
 var livePlatform = new THREE.Mesh(
-  new THREE.BoxGeometry(CELL_WIDTH * 3, CELL_HEIGHT/2, 0),
+  new THREE.BoxGeometry(CELL_WIDTH * 3, CELL_HEIGHT/2, CELL_WIDTH/2),
   new THREE.MeshLambertMaterial( { color: 0xAA0000 } )
 );
 
 var wall = new THREE.Mesh(
-  new THREE.BoxGeometry(CELL_WIDTH/2 + CELL_WIDTH/4, CELL_HEIGHT + CELL_HEIGHT/2, 0),
+  new THREE.BoxGeometry(CELL_WIDTH/2 + CELL_WIDTH/4, CELL_HEIGHT + CELL_HEIGHT/2, CELL_WIDTH/2),
   new THREE.MeshLambertMaterial( { color: 0x00CC55 } )
 );
 
